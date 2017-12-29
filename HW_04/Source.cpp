4c71fbd7bd74e7cab30aa8ece8a1e5d4b821590a
Gennady Portnov <33938041+GD-Portnov@users.noreply.github.com>
2017-12-29 21:35:33 +0300

    HW_04 delayed start((

---
+++ HW_04/Source.cpp
@@ -0,0 +1,18 @@
+//Actual source file on Git Hub:
+//https://github.com/GD-Portnov/HW_04/blob/master/HW_04/Source.cpp
+
+#include <iostream>
+
+int main()
+{
+	int my_array [5]= { 9,3,7,5,1 };
+
+	for (int i = 0; i < 5; i++)
+	{
+
+	}
+
+
+
+}
+
