1f75c2192df69d7f6fd9cb82dc74a83c30c20f0f
Gennady Portnov <33938041+GD-Portnov@users.noreply.github.com>
2017-12-29 22:18:43 +0300

    1

--- HW_04/Source.cpp
+++ HW_04/Source.cpp
@@ -6,13 +6,22 @@
 int main()
 {
 	int my_array [5]= { 9,3,7,5,1 };
+	std::cout << my_array[0] << "\t" << my_array[1] << "\t" << my_array[2] << "\t" << my_array[3] << "\t" << my_array[4] << "\n";
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 4; i++)
 	{
+		int tmp = 0;
+		tmp = my_array[i];
+		if(tmp>my_array[i+1])
+		{
+			my_array[i]=my_array[i+1];
+			my_array[i + 1] = tmp;
+		}
+		std::cout << my_array[0] << "\t" << my_array[1] << "\t" << my_array[2] << "\t" << my_array[3] << "\t" << my_array[4] << "\n";
 
 	}
 
-
+	system("pause");
 
 }
 
