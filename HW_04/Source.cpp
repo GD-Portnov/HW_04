022fe72eef540ceab55e6e6183d933a67cb67ec5
Gennady Portnov <33938041+GD-Portnov@users.noreply.github.com>
2017-12-29 23:53:22 +0300

    2

--- HW_04/Source.cpp
+++ HW_04/Source.cpp
@@ -2,22 +2,49 @@
 //https://github.com/GD-Portnov/HW_04/blob/master/HW_04/Source.cpp
 
 #include <iostream>
+#include <ctime>
+
+#include <limits>
+#include <utility>
+
+static const int MAX = 100;
 
 int main()
 {
-	int my_array [5]= { 9,3,7,5,1 };
-	std::cout << my_array[0] << "\t" << my_array[1] << "\t" << my_array[2] << "\t" << my_array[3] << "\t" << my_array[4] << "\n";
+	int n = 0;
+	int my_array [MAX];
+	std::cout << "n=";
+	std::cin >> n;
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < n; i++)
 	{
-		int tmp = 0;
-		tmp = my_array[i];
-		if(tmp>my_array[i+1])
+		my_array[i] = rand()%1000;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << my_array[i] << "\t";
+			if (i+1 % 10 == 0) { std::cout << "\n"; }
+	}
+	//std::cout << my_array[0] << "\t" << my_array[1] << "\t" << my_array[2] << "\t" << my_array[3] << "\t" << my_array[4] << "\n";
+
+	for (int i = 0; i < n-1; ++i)
+	{
+		for (int i = 0; i < n - 1; ++i)
 		{
-			my_array[i]=my_array[i+1];
-			my_array[i + 1] = tmp;
+			int tmp = my_array[i];
+			if (tmp > my_array[i + 1])
+			{
+				my_array[i] = my_array[i + 1];
+				my_array[i + 1] = tmp;
+			}
 		}
-		std::cout << my_array[0] << "\t" << my_array[1] << "\t" << my_array[2] << "\t" << my_array[3] << "\t" << my_array[4] << "\n";
+
+		for (int i = 0; i < n; i++)
+		{
+			std::cout << my_array[i] << "\t";
+			if (i + 1 % 10 == 0) { std::cout << "\n"; }
+		}
 
 	}
 
